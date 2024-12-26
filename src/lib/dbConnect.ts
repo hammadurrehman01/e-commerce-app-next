617daacbabe6f1252e49dfb117f8c6ea719c2b34
hammadurrehman01 <hammadurrehman1954@gmail.com>
2024-12-26 11:58:29 +0500

    d host updated

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -6,7 +6,7 @@ export const createConnection = async () => {
   try {
     if (!connection) {
       connection = await mysql.createConnection({
-        host: process.env.DATABASE_HOST,
+        host: "127.0.0.1",
         user: process.env.DATABASE_USER,
         password: process.env.DATABASE_PASSWORD,
         database: process.env.DATABASE_NAME,
